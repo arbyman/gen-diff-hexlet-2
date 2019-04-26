cba0f288b5bd3f79792a93d5b8edddf04f9e8d4e
arbyman <bardelorean@yandex.ru>
2019-04-26 16:56:42 +0300

    edit render tree

--- src/renderers/tree.js
+++ src/renderers/tree.js
@@ -20,16 +20,16 @@ const render = (ast, depth = 1) => ast.reduce((acc, item) => {
     case 'added':
       return [...acc, `${currentTab(depth)}+ ${key}: ${stringify(value, depth + tab + 1)}\n`];
     case 'nested':
-      return [...acc, `${currentTab(depth)}  ${key}: {\n`, render(children, depth + tab).join(''), `${currentTab(depth)}  }\n`];
+      return [...acc, `${currentTab(depth)}  ${key}: {\n`, render(children, depth + tab), `${currentTab(depth)}  }\n`];
     case 'unchanged':
       return [...acc, `${currentTab(depth)}  ${key}: ${stringify(value, depth + tab + 1)}\n`];
     case 'deleted':
       return [...acc, `${currentTab(depth)}- ${key}: ${stringify(value, depth + tab + 1)}\n`];
     case 'changed':
-      return [...acc, `${currentTab(depth)}- ${key}: ${stringify(oldValue, depth + tab + 1)}\n${currentTab(depth)}+ ${key}: ${stringify(newValue, depth + tab + 1)}\n`];
+      return [...acc, `${currentTab(depth)}- ${key}: ${stringify(oldValue, depth + tab + 1)}\n`, `${currentTab(depth)}+ ${key}: ${stringify(newValue, depth + tab + 1)}\n`];
     default:
       throw new Error(`${type} - Unknown type node`);
   }
 }, []);
 
-export default ast => `{\n${render(ast).join('')}}`;
+export default ast => `{\n${_.flattenDeep(render(ast)).join('')}}`;
